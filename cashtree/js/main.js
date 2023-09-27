62082e84a406fa91c39fbfbb3dcf753e03217315
SnowCardenas <73762931+SnowCardenas@users.noreply.github.com>
2023-09-26 21:15:05 -0500

    Delete cashtree directory

--- cashtree/js/main.js
+++
@@ -1,8 +0,0 @@
-let list = document.querySelectorAll('.nav li');
-function active(){
-    list.forEach((i) =>
-    i.classList.remove('active'));
-    this.classList.add('active')
-}
-list.forEach((i) => 
-i.addEventListener('click', active))
